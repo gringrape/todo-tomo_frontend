0aa80de97ff96756f7a747d8f717f0b2b4b67774
gringrape <53764714+gringrape@users.noreply.github.com>
2020-07-12 15:42:41 +0900

    [env] webpack additional options
    - open default page when starts
    - add babel-loader rules

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
     compress: true,
     port: 9000,
     hot: true,
-    watchContentBase: true
+    watchContentBase: true,
+    open: true
   },
   module: {
     rules: [
@@ -21,7 +22,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }
